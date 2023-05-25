73b068eba366c90ffc3eeb2e80f1218176452b6f
RiversideRocks <wilest7@gmail.com>
2023-05-25 13:24:41 -0400

    REALLY WORKS

--- work.py
+++ work.py
@@ -10,14 +10,32 @@ def integral1(upper, lower):
 
 def midpoint(function, upper, lower, n_times):
     answer = 0
-    for x in range(n_times + 1):
-        if x == 0:
-            # dummy crap
-            sdjfjksadf = ""
-        else:
-            delta_x = ((float(upper) - float(lower)) / n_times)
-            each = delta_x / 2
-            answer += (delta_x * function(each * x))
+    delta_x = ((float(upper) - float(lower)) / n_times)
+    for x in range(n_times):
+        answer += delta_x * function((delta_x) * (0.5 + x))
     return answer
 
-print(midpoint(functionOne, 2 * math.pi, 0, 8239723))
+def right(function, upper, lower, n_times):
+    answer = 0
+    delta_x = ((float(upper) - float(lower)) / n_times)
+    for x in range(n_times + 1):
+        if x != 0:
+            #print(delta_x * x)
+            answer += delta_x * function(delta_x * x)
+    return answer
+
+def left(function, upper, lower, n_times):
+    answer = 0
+    delta_x = ((float(upper) - float(lower)) / n_times)
+    for x in range(n_times + 1):
+        if x != (n_times + 1):
+            #print(delta_x * x)
+            answer += delta_x * function(delta_x * x)
+    return answer
+
+print(midpoint(functionOne, 123, 0, 2848348))
+print(right(functionOne, 123, 0, 2848348))
+print(left(functionOne, 123, 0, 2848348))
+
+
+#print(trap(functionOne, 2 * math.pi, 0, 4))
